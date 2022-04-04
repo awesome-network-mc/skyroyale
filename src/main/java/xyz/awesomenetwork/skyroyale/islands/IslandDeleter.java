94219b40c0a38e8e70fa02f03285a073734b630a
Dominic Beeson <dominicraebeeson@gmail.com>
2022-04-04 12:16:45 +0100

    use provided block

--- src/main/java/xyz/awesomenetwork/skyroyale/islands/IslandDeleter.java
+++ src/main/java/xyz/awesomenetwork/skyroyale/islands/IslandDeleter.java
@@ -2,6 +2,7 @@ package xyz.awesomenetwork.skyroyale.islands;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 
 import xyz.awesomenetwork.schematics.SchematicPasteCallback;
@@ -11,8 +12,8 @@ public class IslandDeleter implements SchematicPasteCallback {
 	private final BlockData AIR = Material.AIR.createBlockData();
 
 	@Override
-	public boolean blockPaste(String pasteId, BlockData block, Location pasteCentre, Location absoluteLocation, LocationNoWorld relativeLocation) {
-		absoluteLocation.getWorld().setBlockData(absoluteLocation, AIR);
+	public boolean blockPaste(String pasteId, Block block, BlockData blockdata, Location pasteCentre, Location absoluteLocation, LocationNoWorld relativeLocation) {
+		block.setBlockData(AIR);
 		return false;
 	}
 }
