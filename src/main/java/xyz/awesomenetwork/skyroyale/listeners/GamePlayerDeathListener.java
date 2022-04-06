0792009799f2489c183f6966754d918b1bbd8589
Dominic Beeson <dominicraebeeson@gmail.com>
2022-04-06 18:08:55 +0100

    add players to leaderboard on death

--- src/main/java/xyz/awesomenetwork/skyroyale/listeners/GamePlayerDeathListener.java
+++ src/main/java/xyz/awesomenetwork/skyroyale/listeners/GamePlayerDeathListener.java
@@ -6,17 +6,26 @@ import org.bukkit.event.Listener;
 
 import xyz.awesomenetwork.minigametemplate.GameManager;
 import xyz.awesomenetwork.minigametemplate.events.GamePlayerDeathEvent;
+import xyz.awesomenetwork.skyroyale.leaderboard.SkyRoyaleLeaderboard;
 
 public class GamePlayerDeathListener implements Listener {
 	private final GameManager gameManager;
+	private final SkyRoyaleLeaderboard leaderboard;
 
-	public GamePlayerDeathListener(GameManager gameManager) {
+	public GamePlayerDeathListener(GameManager gameManager, SkyRoyaleLeaderboard leaderboard) {
 		this.gameManager = gameManager;
+		this.leaderboard = leaderboard;
 	}
 
 	@EventHandler
 	public void gamePlayerDeath(GamePlayerDeathEvent e) {
 		int playersRemaining = gameManager.getIngamePlayers().size() - 1;
 		e.setDeathMessage(e.getDeathMessage() + ChatColor.GRAY + " (" + playersRemaining + "/" + gameManager.getOptions().maxPlayers + ")");
+
+		leaderboard.setPosition(playersRemaining, e.getVictim().getName());
+		if (playersRemaining == 1) {
+			String winner = gameManager.getIngamePlayers().iterator().next().getName();
+			leaderboard.setPosition(0, winner);
+		}
 	}
 }
