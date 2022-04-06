6c804afc7df1501543b1bd7038347ee53dc306c5
Dominic Beeson <dominicraebeeson@gmail.com>
2022-04-06 13:03:13 +0100

    new event and update params

--- src/main/java/xyz/awesomenetwork/skyroyale/SkyRoyale.java
+++ src/main/java/xyz/awesomenetwork/skyroyale/SkyRoyale.java
@@ -23,6 +23,7 @@ import xyz.awesomenetwork.skyroyale.listeners.GamePlayerJoinListener;
 import xyz.awesomenetwork.skyroyale.listeners.GamePlayerLeaveListener;
 import xyz.awesomenetwork.skyroyale.listeners.GamePlayerSpectateListener;
 import xyz.awesomenetwork.skyroyale.listeners.GameStartListener;
+import xyz.awesomenetwork.skyroyale.listeners.ItemSpawnListener;
 import xyz.awesomenetwork.skyroyale.maps.MapManager;
 
 public class SkyRoyale extends JavaPlugin {
@@ -75,6 +76,7 @@ public class SkyRoyale extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new GamePlayerJoinListener(islandManager), this);
         getServer().getPluginManager().registerEvents(new GamePlayerLeaveListener(islandManager), this);
         getServer().getPluginManager().registerEvents(new GamePlayerSpectateListener(islandWorld, skyRoyaleConfig.getIslandSpawnBoxY()), this);
-        getServer().getPluginManager().registerEvents(new GameStartListener(this, islandManager, schematicHandler, spawnBoxSchematic, islandWorld, skyRoyaleConfig), this);
+        getServer().getPluginManager().registerEvents(new GameStartListener(this, islandManager, schematicHandler, spawnBoxSchematic, islandWorld, skyRoyaleConfig, chestConfig), this);
+        getServer().getPluginManager().registerEvents(new ItemSpawnListener(), this);
     }
 }
