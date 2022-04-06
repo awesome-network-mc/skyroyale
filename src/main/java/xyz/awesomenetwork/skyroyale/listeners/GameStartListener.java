6e1db31a142209bf76367f650944889affcfcb65
Dominic Beeson <dominicraebeeson@gmail.com>
2022-04-06 13:12:57 +0100

    crumble start is scheduled task instead of listening for game timer seconds

--- src/main/java/xyz/awesomenetwork/skyroyale/listeners/GameStartListener.java
+++ src/main/java/xyz/awesomenetwork/skyroyale/listeners/GameStartListener.java
@@ -61,10 +61,10 @@ public class GameStartListener implements Listener {
 
 		int amountOfIslands = islandManager.getIslands().size();
 		// This is a magic formula that someone I was working with wrote ~5 years ago. It was designed to modify the island crumble start time based on how many players the game started with, and to make the game feel fast paced no matter the size. It was only designed for up to 100 people.
-		int crumbleStartSeconds = (int) Math.floor(21.3 + Math.sqrt(100 * (amountOfIslands - 1)) + (2.545 / (2 * Math.pow(10, 15))) * Math.pow(amountOfIslands, 8) - (13 / (104 * Math.pow(10, 6))) * Math.pow(amountOfIslands, 4)) + 29;
+		int crumbleStartTicks = (int) (Math.floor(21.3 + Math.sqrt(100 * (amountOfIslands - 1)) + (2.545 / (2 * Math.pow(10, 15))) * Math.pow(amountOfIslands, 8) - (13 / (104 * Math.pow(10, 6))) * Math.pow(amountOfIslands, 4)) + 29) * 20;
 
 		// When time hits 12000, islands start to crumble as it turns to night
-		islandWorld.setTime(12000 - (crumbleStartSeconds * 20));
+		islandWorld.setTime(12000 - (crumbleStartTicks));
 		islandWorld.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true);
 
 		// Set initial world border
@@ -77,11 +77,15 @@ public class GameStartListener implements Listener {
 		islandWorld.getWorldBorder().setCenter(0.0, 0.0);
 		islandWorld.getWorldBorder().setSize(distance * 2);
 
-		plugin.getServer().getPluginManager().registerEvents(new GameRunningTimeListener(islandManager, islandWorld, skyRoyaleConfig), plugin);
+		// Create task to start island crumbling and world border shrinking
+		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+			islandManager.crumbleIslands();
+			islandWorld.getWorldBorder().setSize(skyRoyaleConfig.getDistanceBetweenIslands() * 2, skyRoyaleConfig.getIslandCrumbleSpeed() / 20);
+		}, crumbleStartTicks);
 
 		// Create scheduled tasks for supply drops
-		int supplyDrop1Ticks = (crumbleStartSeconds * 20) / 3;
-		int supplyDrop2Ticks = (int) ((crumbleStartSeconds * 20) / 1.5);
+		int supplyDrop1Ticks = (crumbleStartTicks) / 3;
+		int supplyDrop2Ticks = (int) ((crumbleStartTicks) / 1.5);
 
 		LootTier lootTier2 = chestConfig.getTier(2);
 		LootTier lootTier3 = chestConfig.getTier(3);
