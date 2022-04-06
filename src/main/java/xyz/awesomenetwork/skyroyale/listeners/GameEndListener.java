7ca48e56f0e9a5fd2e1fe1ab24bd378983f85916
Dominic Beeson <dominicraebeeson@gmail.com>
2022-04-06 18:15:33 +0100

    [WIP] testing broadcast leaderboard at end of game

---
+++ src/main/java/xyz/awesomenetwork/skyroyale/listeners/GameEndListener.java
@@ -0,0 +1,27 @@
+package xyz.awesomenetwork.skyroyale.listeners;
+
+import org.bukkit.Server;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import xyz.awesomenetwork.minigametemplate.events.GameEndEvent;
+import xyz.awesomenetwork.skyroyale.leaderboard.SkyRoyaleLeaderboard;
+
+public class GameEndListener implements Listener {
+	private final Server server;
+	private final SkyRoyaleLeaderboard leaderboard;
+
+	public GameEndListener(Server server, SkyRoyaleLeaderboard leaderboard) {
+		this.server = server;
+		this.leaderboard = leaderboard;
+	}
+
+	@EventHandler
+	public void gameEnd(GameEndEvent e) {
+		server.broadcastMessage("Podium:");
+		for (int i = 0; i < 3; i++) {
+			String name;
+			if ((name = leaderboard.getPosition(i)) != null) server.broadcastMessage(" " + i + ": " + name);
+		}
+	}
+}
