da91921270dbfc980d7c5a28b4257d39edd3feb3
Dominic Beeson <dominicraebeeson@gmail.com>
2022-04-08 15:41:51 +0100

    provide loot tier

--- src/main/java/xyz/awesomenetwork/skyroyale/listeners/GameStartListener.java
+++ src/main/java/xyz/awesomenetwork/skyroyale/listeners/GameStartListener.java
@@ -80,7 +80,7 @@ public class GameStartListener implements Listener {
 
 	@EventHandler
 	public void gameStart(GameStartEvent e) {
-		islandManager.populateAllIslandChests(1);
+		islandManager.populateAllIslandChests(chestConfig.getTier(1));
 
 		islandManager.getIslands().forEach(island -> {
 			schematicHandler.pasteSchematic(new SchematicPasteOptions(ISLAND_SPAWN_BOX_SCHEMATIC, islandManager.getIslandSpawnBoxCentre(island.getIslandNumber()), 0, islandDeleter, 0));
